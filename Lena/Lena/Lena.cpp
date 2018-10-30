defaa08087ce5fea022e766f305d26933c250ae7
croook <41216826+croook@users.noreply.github.com>
2018-10-30 22:18:06 +0800

    class4

--- Lena/Lena/Lena.cpp
+++ Lena/Lena/Lena.cpp
@@ -48,19 +48,23 @@ float kernel6[5][5] = {
 	0.0120, 0.1253, 0.2736, 0.1253, 0.0120
 };
 
-GByte* buffTmp, **buff;
+GByte **buff , **buff2;
 
+int N = 6;
 int cou[6] = { 1,2,1,1,1,2 };
 
-int calc(int num, int row, int col,int buffnum,int tmpXlen) {
-	int n = cou[num - 1];
+int calc(int row, int col,int buffnum,int tmpXlen) {
+	int n = cou[N - 1];
 	float sum = 0;
 
 	for (int i = 0; i < 2*n+1; i++) {
 		for (int j = 0; j < 2*n+1; j++) {
-			sum += buff[buffnum][(row-n+i)*tmpXlen+(col-n+j)] * kernel3[i][j];
+			sum += buff[buffnum][(row-n+i)*tmpXlen+(col-n+j)] * kernel6[i][j];
 		}
 	}
+
+	if (sum < 0) sum = 0;
+	else if (sum > 255) sum = 255;
 	return int(sum);
 }
 
@@ -87,8 +91,6 @@ int main()
 	imgYlen = poSrcDS->GetRasterYSize();
 	bandNum = poSrcDS->GetRasterCount();
 
-	buffTmp = (GByte*)CPLMalloc(imgXlen * imgYlen * sizeof(GByte));
-
 	poDstDS = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(
 		dstPath, imgXlen, imgYlen, bandNum, GDT_Byte, NULL
 	);
@@ -97,34 +99,44 @@ int main()
 	for (int i = 0; i < bandNum; i++) {
 		buff[i] = (GByte*)CPLMalloc(imgXlen*imgYlen * sizeof(GByte));
 	}
+	buff2 = (GByte**)CPLMalloc(bandNum*imgXlen*imgYlen * sizeof(GByte));
+	for (int i = 0; i < bandNum; i++) {
+		buff2[i] = (GByte*)CPLMalloc(imgXlen*imgYlen * sizeof(GByte));
+	}
 
 	for (int i = 0; i < bandNum; i++) {
 		poSrcDS->GetRasterBand(i+1)->RasterIO(
 			GF_Read, 0, 0, imgXlen, imgYlen, buff[i], imgXlen, imgYlen, GDT_Byte, 0, 0
 		);
+		poSrcDS->GetRasterBand(i + 1)->RasterIO(
+			GF_Read, 0, 0, imgXlen, imgYlen, buff2[i], imgXlen, imgYlen, GDT_Byte, 0, 0
+		);
 	}
 
 	for (int k = 0; k < bandNum; k++) {
-		for (int j = 1; j < imgYlen-1 ; j++) {
-			for (int i = 1; i < imgXlen-1; i++) {
-				buff[k][j*imgXlen + i] = (GByte)calc(3, j, i,k,imgXlen);
+		for (int j = cou[N-1]; j < imgYlen-N ; j++) {
+			for (int i = cou[N-1]; i < imgXlen-N; i++) {
+				buff2[k][j*imgXlen + i] = (GByte)calc(j, i,k,imgXlen);
 			}
 		}
 	}
 
 	for (int i = 0; i < bandNum; i++) {
 		poDstDS->GetRasterBand(i + 1)->RasterIO(
-			GF_Write, 0, 0, imgXlen, imgYlen, buff[i], imgXlen, imgYlen, GDT_Byte, 0, 0
+			GF_Write, 0, 0, imgXlen, imgYlen, buff2[i], imgXlen, imgYlen, GDT_Byte, 0, 0
 		);
 		printf("... ... band %d processing ... ...\n", i);
 	}
 
 
-	CPLFree(buffTmp);
 	for (int i = 0; i < bandNum; i++) {
 		CPLFree(buff[i]);
 	}
 	CPLFree(buff);
+	for (int i = 0; i < bandNum; i++) {
+		CPLFree(buff2[i]);
+	}
+	CPLFree(buff2);
 	GDALClose(poDstDS);
 	GDALClose(poSrcDS);
 
